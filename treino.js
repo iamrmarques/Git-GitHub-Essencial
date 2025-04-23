a85ae5d2555b392ddfc40b875abeed98168253e0
iamrmarques <roberthmarques123@outlook.com>
2025-04-23 12:21:01 -0300

    Mais alterações no JS

--- treino.js
+++ treino.js
@@ -1,6 +1,14 @@
 let nome = "Robert"
 let idade = 28
+
 let resultado = idade > 18 && nome == "Robert"
+
+console.log(resultado)
+
+let valor = 10
+let valor2 = 20
+
+let resultado2 = valor > valor2
 
 console.log(resultado)
 
