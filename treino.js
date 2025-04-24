15698970302c06a9d2e054178a979e53b0dc5167
iamrmarques <roberthmarques123@outlook.com>
2025-04-24 16:44:55 -0300

    Mais exemplos adicionados em JS.

--- treino.js
+++ treino.js
@@ -5,10 +5,47 @@ let resultado = idade > 18 && nome == "Robert"
 
 console.log(resultado)
 
+
+///
+
+
 let valor = 10
 let valor2 = 20
 
 let resultado2 = valor < valor2
 
 console.log(resultado2)
+
+
+///
+
+
+let media = 5
+if (media >= 7) {
+    console.log("Passou")
+}else {
+    console.log("Reprovado")
+}
+
+
+///
+
+
+let opcao = 3
+  switch(opcao) {
+case 1: 
+    console.log("Saldo Positivo")
+   break
+case 2:
+    console.log("Saldo Negativo")
+   break
+case 3:
+    console.log("Central")
+    break
+default:
+    console.log("Opção Inválida")
+}
+
+
+///
 
